07f1b1828089821567d21273177bd4181ec56e7a
Piero Chavez <you@example.com>
2024-12-03 01:24:44 -0500

    contacto page

--- Front/src/pages/Contacto/ContactoPage.jsx
+++ Front/src/pages/Contacto/ContactoPage.jsx
@@ -1,38 +1,48 @@
 // src/pages/ContactPage.jsx
-import React from 'react';
-import './ContactoPage.css';
-
+import React from "react";
+import "./ContactoPage.css";
 
 const ContactPage = () => {
   return (
     <div className="contact-page">
-      <h1>Contacto</h1>
-     
+      <div className="main">
+        
 
-      
+        <div className="content">
+          <h1>
+            Web Design & <br />
+            <span>Development</span> <br />
+            Course
+          </h1>
+          <p className="par">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt neque
+            expedita atque eveniet <br /> quis nesciunt. Quos nulla vero
+            consequuntur, fugit nemo ad delectus <br /> a quae totam ipsa illum
+            minus laudantium?
+          </p>
+
+          <button className="cn">
+            <a href="#">JOIN US</a>
+          </button>
+
+          <div className="form">
+            <h2>Login Here</h2>
+            <input type="email" placeholder="Enter Email Here" />
+            <input type="password" placeholder="Enter Password Here" />
+            <button className="btnn">
+              <a href="#">Login</a>
+            </button>
+
+            <p className="link">
+              Don't have an account<br />
+              <a href="#">Sign up</a> here
+            </p>
+            <p className="liw">Log in with</p>
 
 
-
-
-        <div className="contact-form">
-          <h2>Contáctanos</h2>
-          <form>
-            <div className="form-group">
-              <label>Nombre</label>
-              <input type="text" placeholder="Ingresa tu nombre" />
-            </div>
-            <div className="form-group">
-              <label>Email</label>
-              <input type="email" placeholder="Ingresa tu email" />
-            </div>
-            <div className="form-group">
-              <label>Mensaje</label>
-              <textarea placeholder="Escribe tu mensaje"></textarea>
-            </div>
-            <button className='contacto-contacto' type="submit">Enviar</button>
-          </form>
+          </div>
         </div>
-      
+      </div>
     </div>
   );
 };
